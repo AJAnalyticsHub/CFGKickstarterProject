370e315873c2dff4da16ab88000356992288ac78
ajames9277 <angharadejames@icloud.com>
2024-07-21 20:17:13 +0100

    Update MoonPigeon (view, trigger).sql

--- MoonPigeon (view, trigger).sql
+++ MoonPigeon (view, trigger).sql
@@ -19,28 +19,25 @@ WHERE town = 'Glasgow'
 ORDER BY vw_pa.first_name;
 
 -- a trigger to standardise customer informtion --
+
 DELIMITER //
 CREATE TRIGGER fix_customer_info
 BEFORE INSERT ON Customers
 FOR EACH ROW
 BEGIN
-	SET NEW.first_name = CONCAT(UPPER(SUBSTRING(NEW.first_name,1,1)), 
-						LOWER(SUBSTRING(NEW.first_name FROM 2)));
-    SET NEW.last_name = CONCAT(UPPER(SUBSTRING(NEW.last_name,1,1)), 
-						LOWER(SUBSTRING(NEW.last_name FROM 2)));
+	SET NEW.first_name = CONCAT(UPPER(SUBSTRING(NEW.first_name,1)), 
+		LOWER(SUBSTRING(NEW.first_name FROM 2)));
+	SET NEW.last_name = CONCAT(UPPER(SUBSTRING(NEW.last_name,1)), 
+		LOWER(SUBSTRING(NEW.last_name FROM 2)));
 	SET NEW.email = LCASE(NEW.email);
-	SET NEW.street = CONCAT(UPPER(SUBSTRING(NEW.street,1,1)), 
-						LOWER(SUBSTRING(NEW.street FROM 2)));
-    SET NEW.town = CONCAT(UCASE(LEFT(NEW.town,1)), 
-					LCASE(SUBSTRING(NEW.town, 2)));
-    SET NEW.postcode = UCASE(NEW.postcode);
+	SET NEW.postcode = UCASE(NEW.postcode);
 END;
 //
 DELIMITER ;
 
 INSERT INTO Customers VALUES
 (101, 'james', 'smith', 'James.Smith@example.com', 23, 
-'baker street', 'london', 'nw1 6xe', 51.5237, -0.1585);
+'Baker street', 'London', 'nw1 6xe', 51.5237, -0.1585);
 
 DELETE FROM Customers WHERE customer_id=101; -- to demonstrate --
 
@@ -51,7 +48,10 @@ DELETE FROM Customers WHERE customer_id=101; -- to demonstrate --
 -- Would it be easier to understand if we put which section the gift comes from rather than its id?
 
 CREATE VIEW vw_package_adress AS
-SELECT s.sales_id, c.first_name, c.last_name, c.house_number, c.street, c.town, c.postcode, g.giftsection, g.gifttype
+SELECT s.sales_id, c.first_name, 
+	c.last_name, c.house_number, 
+	c.street, c.town, c.postcode, 
+	g.giftsection, g.gifttype
 FROM Sales s
 INNER JOIN Customers c ON s.customer_id = c.customer_id
 INNER JOIN Gift g ON s.gift_id = g.gift_id
